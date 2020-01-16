13abab9130ecf7e687180ef78f358b670e282393
Krzysztof Szypulski <krzychu700@gmail.com>
2020-01-16 14:38:06 +0100

    connected to NASA  API

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "vue/max-attributes-per-line": "off"
   },
   parserOptions: {
     parser: 'babel-eslint',
@@ -20,6 +21,9 @@ module.exports = {
         '**/__tests__/*.{j,t}s?(x)',
         '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
+      rules: {
+        "vue/max-attributes-per-line": "off"
+      },
       env: {
         jest: true,
       },
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,12 +1,73 @@
 <template>
-  <div class="Search">
-    <h1>spacer</h1>
+  <div :class="$style['search']">
+    <div :class="$style['inputContainer']">
+      <label for="search">Search</label>
+      <input
+        name="search"
+        id="search"
+        :class="$style['inputContainer__input']"
+        v-model="searchValue"
+        @input="handleInput"
+      />
+      <ul>
+        <li v-for="item in results" :key="item.data[0].nasa_id">
+          <p>{{ item.data[0].description}} </p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 
 <script>
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+
+const API = 'https://images-api.nasa.gov/search';
+
 export default {
   name: 'Search',
+  data() {
+    return {
+      searchValue: '',
+      results: [],
+    };
+  },
+  methods: {
+    // eslint-disable-next-line
+    handleInput: debounce(function () {
+      axios.get(`${API}?q=${this.searchValue}&media_type=image`).then((respone) => {
+        this.results = respone.data.collection.items;
+      }).catch((err) => {
+        console.log(err);
+      });
+    }, 500),
+  },
 };
 </script>
+
+
+<style module lang="scss">
+  .search {
+    margin: 0;
+    padding: 30px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .inputContainer {
+      display: flex;
+      flex-direction: column;
+      width: 300px;
+      font-family: Montserrat, sans-serif;
+
+      .inputContainer__input {
+        height: 30px;
+        border: 0;
+        border-bottom: 1px solid black;
+      }
+    }
+  }
+
+</style>
